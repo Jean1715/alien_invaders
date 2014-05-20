34917d6a16fcaa6c35325bd379c4e2649350e228
apple <apple@mac27769.staff.bournemouth.ac.uk>
2014-05-20 11:57:45 +0100

    Added comment

--- js/game.js
+++ js/game.js
@@ -77,7 +77,7 @@ Alien.prototype.step = function(dt) {
 }
 
 Alien.prototype.fireSometimes = function() {
-      if(Math.random()*100 < 10) {
+      if(Math.random()*100 < 10) /*How often aliens fire*/ { 
         this.board.addSprite('missile',this.x + this.w/2 - Sprites.map.missile.w/2,
                                       this.y + this.h, 
                                      { dy: 100 });
